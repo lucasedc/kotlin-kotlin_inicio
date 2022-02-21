eea7f6629554c4ca4e4977f162d72c2b4428a7e1
Lucas Carvalho <lucaseduardo@gmail.com>
2022-02-21 11:14:42 -0300

    Teste Unitários

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,20 @@
+import foo.portaria
+import org.junit.Assert
+import org.junit.Assume
+import org.junit.Test
+
+
+class MainTest{
+
+    @Test
+    fun portariaTest(){
+        Assert.assertEquals(portaria(15,"",""),"Negado.")
+        Assert.assertEquals(portaria(20,"indeterminado",""),"Negado.")
+        Assert.assertEquals(portaria(20,"comum",""),"Negado.")
+        Assert.assertEquals(portaria(20,"comum","xt0900"),"Welcome.")
+        Assert.assertEquals(portaria(20,"premium","xl"),"Welcome.")
+        Assert.assertEquals(portaria(20,"premium","xt0900"),"Negado.")
+    }
+
+
+}
